e54fccc2106c9e2d4dbdd53546b4ce597a2cc792
Renan Costa Filgueiras <renan2727@hotmail.com>
2022-04-08 19:49:15 -0300

    ResolucaoPratica

--- src/Aula06/TabuadaSimplificada.java
+++ src/Aula06/TabuadaSimplificada.java
@@ -2,11 +2,11 @@ package Aula06;
 
 public class TabuadaSimplificada {
 	private int numero;
-	
+
 	public TabuadaSimplificada() {
-		
+
 	}
-	
+
 	public int getNumero() {
 		return numero;
 	}
@@ -18,33 +18,32 @@ public class TabuadaSimplificada {
 	public void soma() {
 		operacaoBasica("Soma", '+');
 	}
-	
+
 	public void subtracao() {
 		operacaoBasica("Subtração", '-');
 	}
-	
+
 	public void multiplicacao() {
 		operacaoBasica("Multiplicação", '*');
 	}
-	
+
 	public void divisao() {
 		operacaoBasica("Divisão", '/');
 	}
-	
+
 	private void operacaoBasica(String nome, char simbolo) {
-		System.out.println("Tabuada "+nome);
-		for(int i=1;i<11;i++) {
-			System.out.print(numero+" "+simbolo+" "+i+" = ");
-			if(simbolo=='+') {
-				System.out.println(numero+i);
-			} else if (simbolo=='-') {
-				System.out.println(numero-i);
-			} else if(simbolo=='/') {
-				System.out.println(numero/i);
-			}else {
-				System.out.println(numero*i);
+		System.out.println("Tabuada " + nome);
+		for (int i = 1; i < 11; i++) {
+			System.out.print(numero + " " + simbolo + " " + i + " = ");
+			if (simbolo == '+') {
+				System.out.println(numero + i);
+			} else if (simbolo == '-') {
+				System.out.println(numero - i);
+			} else if (simbolo == '/') {
+				System.out.println(numero / i);
+			} else {
+				System.out.println(numero * i);
 			}
 		}
 	}
 }
-
